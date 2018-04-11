ed60192c487a8b51cb36e0079d491f75aa7f9dd7
Jason Pamental <jpamental@isovera.com>
2018-04-11 14:14:09 +0200

    Paring down gulp file and updating readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # rwt-vf-workshop-short
 Working files for the shortened version of my workshop on using variable fonts and modern CSS techniques.
+
+## Initial setup
+To make use of the `gulp` process, you need to have `npm` and `gulp` installed.
+
+Once both are installed, follow these steps:
+Open a terminal window and navigate to the project root
+Execute the command `npm install`
+
+## Running the `gulp` process
+If the initial setup has been completed, executing the command `gulp` from the root of the project will start the 'watch' task and the local server with BrowserSync.
--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
-var useref = require('gulp-useref');
-var uglify = require('gulp-uglify');
-var gulpIf = require('gulp-if');
-var cssnano = require('gulp-cssnano');
-var imagemin = require('gulp-imagemin');
-var cache = require('gulp-cache');
-var del = require('del');
 var runSequence = require('run-sequence');
-
-// Basic Gulp task syntax
-gulp.task('hello', function() {
-  console.log('Hello Zell!');
-})
 
 // Development Tasks 
 // -----------------
@@ -43,46 +31,6 @@ gulp.task('watch', function() {
   gulp.watch('js/**/*.js', browserSync.reload);
 })
 
-// Optimization Tasks 
-// ------------------
-
-// Optimizing CSS and JavaScript 
-gulp.task('useref', function() {
-
-  return gulp.src('*.html')
-    .pipe(useref())
-    .pipe(gulpIf('*.js', uglify()))
-    .pipe(gulpIf('*.css', cssnano()))
-    .pipe(gulp.dest('dist'));
-});
-
-// Optimizing Images 
-gulp.task('images', function() {
-  return gulp.src('images/**/*.+(png|jpg|jpeg|gif|svg)')
-    // Caching images that ran through imagemin
-    .pipe(cache(imagemin({
-      interlaced: true,
-    })))
-    .pipe(gulp.dest('dist/images'))
-});
-
-// Copying fonts 
-gulp.task('fonts', function() {
-  return gulp.src('fonts/**/*')
-    .pipe(gulp.dest('dist/fonts'))
-})
-
-// Cleaning 
-gulp.task('clean', function() {
-  return del.sync('dist').then(function(cb) {
-    return cache.clearAll(cb);
-  });
-})
-
-gulp.task('clean:dist', function() {
-  return del.sync(['dist/**/*', '!dist/images', '!dist/images/**/*']);
-});
-
 // Build Sequences
 // ---------------
 
@@ -94,9 +42,7 @@ gulp.task('default', function(callback) {
 
 gulp.task('build', function(callback) {
   runSequence(
-    'clean:dist',
     'sass',
-    ['useref', 'images', 'fonts'],
     callback
   )
 })
